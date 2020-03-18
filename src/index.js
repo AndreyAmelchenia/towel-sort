1d09d13aa67c61d600948af845a811c3eb568bf0
AndreyAmelchenia <60568794+AndreyAmelchenia@users.noreply.github.com>
2020-03-18 17:39:36 +0300

    docs:proba

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  
+  matrix.forEach(element => {
+    if (Array.isArray(element)){
+      element.forEach(el => {
+        arr.push(el);
+      })
+    } else {
+      arr.push(element)
+    }
+  });
+  console.log(arr);
+  return arr.sort((a, b) => a - b );
 }
